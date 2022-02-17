7550dfe2123b624b023149725cf62a0dd3c36df3
PJWitt <pwitt0068@gmail.com>
2022-02-17 18:27:32 -0500

    part 1 complete

---
+++ index.js
@@ -0,0 +1,15 @@
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+      FROM users;
+    `
+  );
+
+  return rows;
+}
+
+// and export them
+module.exports = {
+  client,
+  getAllUsers,
+};
