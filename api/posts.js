13728f5f5e6465f9ff385f78065fb6448bfcdaab
PJWitt <pwitt0068@gmail.com>
2022-02-26 09:32:15 -0500

    stage 3

--- api/posts.js
+++ api/posts.js
@@ -4,6 +4,35 @@ const postsRouter = express.Router();
 // NEW
 const { getAllPosts } = require("../db");
 
+const { requireUser } = require("./utils");
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    if (post) {
+      res.send({ post });
+    }
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 // UPDATE
 postsRouter.get("/", async (req, res) => {
   const posts = await getAllPosts();
