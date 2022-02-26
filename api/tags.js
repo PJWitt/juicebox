cdbf643eed7fe30849f2ad5a2a31def8f6bd7bec
PJWitt <pwitt0068@gmail.com>
2022-02-26 10:06:11 -0500

    Finished project

--- api/tags.js
+++ api/tags.js
@@ -2,7 +2,7 @@ const express = require("express");
 const tagsRouter = express.Router();
 
 // NEW
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 // UPDATE
 tagsRouter.get("/", async (req, res) => {
@@ -11,6 +11,23 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+  try {
+    // use our method to get posts by tag name from the db
+    const results = await getPostsByTagName(tagName);
+
+    // send out an object to the client { posts: // the posts }
+    res.send({
+      posts: results,
+    });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
